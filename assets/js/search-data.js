3ca99debb6e34cedc60c81f6c8e30454e6b8fc54
twin4ch <twin4ch.insa@gmail.com>
2025-09-08 12:46:30 +0000

    Deploying to gh-pages from @ twin4ch/twin4ch.github.io@9ec05aa12d617328fdb7baf7bcc8857f4f531b1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,11 +51,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
