c56877378c7524442a29827ebd27dcd40cd1bae7
twin4ch <twin4ch.insa@gmail.com>
2025-09-08 17:58:20 +0000

    Deploying to gh-pages from @ twin4ch/twin4ch.github.io@3e6799c3bf4033a3535f53d0b62c06674915de67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
