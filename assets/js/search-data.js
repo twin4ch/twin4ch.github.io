6c375814dad8034921d76c152284ca8c1edbc34d
twin4ch <twin4ch.insa@gmail.com>
2025-09-08 13:20:33 +0000

    Deploying to gh-pages from @ twin4ch/twin4ch.github.io@426822a901b3ca2f10eb7bb233e4b62922b10280 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,8 +46,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-gaussian-splatting-for-heritage",
+          title: 'Gaussian splatting for heritage',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
