db5cd8fafb32abcc06cd75ea2a18d77997195637
twin4ch <twin4ch.insa@gmail.com>
2026-08-15 13:11:55 +0000

    Deploying to gh-pages from @ twin4ch/twin4ch.github.io@2472bd231862d95210054774ccfdfc62f063cc58 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-people",
+          title: "people",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "",
           section: "Navigation",
